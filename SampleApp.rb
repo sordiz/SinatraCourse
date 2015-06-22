9cbd7976fa5345887865f2b93c1da2c57c1eb8d3
sordiz <sordiz@gmail.com>
2015-06-21 23:38:34 -0300

    Sinatra course initial commit.

---
+++ SampleApp.rb
@@ -0,0 +1,42 @@
+require 'mongo'
+require 'shotgun'
+require 'sinatra'
+
+configure do
+  enable :sessions
+  client = Mongo::Client.new(["127.0.0.1:27017"], database: "test")
+  set :mongo, client
+end
+
+class SampleApp < Sinatra::Application
+
+  before do
+    p "REQUEST FROM: " + request.host
+  end
+
+  get '/sumar/:a/:b' do
+    (params[:a].to_i + params[:b].to_i).to_s
+  end
+
+  post '/add_post' do
+    settings.mongo[:posts].insert_one(
+      {"title"      => params[:title], 
+       "body"       => params[:body],
+       "created_at" => Time.now})
+
+    redirect '/'
+  end
+
+  get '/' do
+    @posts = []
+    settings.mongo[:posts].find.each do |doc|
+      doc.delete_if {|key, value| key == "_id" }
+      @posts << doc
+    end
+    erb 'posts'.to_sym
+  end
+ 
+  after do
+    p "RESPONSE: " + response.to_s
+  end 
+end
---
+++ config.ru
@@ -0,0 +1,2 @@
+require './SampleApp'
+run SampleApp
